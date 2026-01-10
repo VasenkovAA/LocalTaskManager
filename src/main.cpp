5a479cca501faf43df9e52b6734e81707ea6413a
Andrey <andrej.500@mail.ru>
2026-01-10 23:45:20 +0300

    fix: db loaded

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,22 @@
 #include "db/database.hxx"
 #include "gui/main_window.hxx"
 
-int main(int argc, char* argv[])
-{
-  try
-  {
+int main(int argc, char *argv[]) {
+  try {
     QApplication app(argc, argv);
     QApplication::setOrganizationName("LocalTaskManager");
     QApplication::setApplicationName("LocalTaskManager");
 
     QCommandLineParser parser;
-    parser.setApplicationDescription("LocalTaskManager (GUI)");
     parser.addHelpOption();
 
-    QCommandLineOption configOpt(QStringList{"c", "config"},
-                                 "Path to ini config file (default: рядом с программой).",
-                                 "path");
-    QCommandLineOption dbOpt(QStringList{"d", "db"},
-                             "Path to sqlite database file. If set, it will be saved to ini.",
-                             "path");
+    QCommandLineOption configOpt(
+        QStringList{"c", "config"},
+        "Path to ini config file (default: рядом с программой).", "path");
+    QCommandLineOption dbOpt(
+        QStringList{"d", "db"},
+        "Path to sqlite database file. If set, it will be saved to ini.",
+        "path");
 
     parser.addOption(configOpt);
     parser.addOption(dbOpt);
@@ -37,19 +35,22 @@ int main(int argc, char* argv[])
     const QString cliDbPath = parser.value(dbOpt).trimmed();
 
     const AppSettings st = loadOrCreateSettings(iniPath, cliDbPath);
+
     auto opened = db::openDatabaseReadOrCreate(st.dbPath);
 
-    db::ensureSchemaBestEffort(*opened.db);
-    db::seedIfEmpty(*opened.db);
+    if (opened.createdNew) {
+      db::ensureSchemaBestEffort(*opened.db);
+      db::seedIfEmpty(*opened.db);
+    } else {
+
+      db::seedIfEmpty(*opened.db);
+    }
 
     MainWindow w(opened.db);
     w.show();
 
     return app.exec();
-  }
-  catch (const std::exception& e)
-  {
-    // без QMessageBox: если упали до окна, просто stderr
+  } catch (const std::exception &e) {
     fprintf(stderr, "Fatal error: %s\n", e.what());
     return 1;
   }
